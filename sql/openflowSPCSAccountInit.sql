0baa1ed69f20f30cc9ae809bdaa0e1a375eda1b2
Dan Chaffelson <dan.chaffelson@snowflake.com>
2025-10-03 14:03:03 +0100

    Pruned Notebook set. Added gitignore. Updated Notebook set to latest versions

--- sql/openflowSPCSAccountInit.sql
+++
@@ -1,9 +0,0 @@
--- This script prepares a workspace for Openflow SPCS Notebooks
--- Set the role to ACCOUNTADMIN for creating top-level objects
-USE ROLE ACCOUNTADMIN;
-
--- Attach to the Openflow Database
-CREATE DATABASE IF NOT EXISTS OPENFLOW;
-USE DATABASE OPENFLOW;
-CREATE SCHEMA IF NOT EXISTS OPENFLOW;
-USE SCHEMA OPENFLOW;
